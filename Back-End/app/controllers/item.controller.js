7ff7f74586853332fa0ccc57bbfd8bb9fd4aedfb
santoshelectem <santosh@electems.com>
2021-05-27 18:37:34 +0530

    Search by name

--- Back-End/app/controllers/item.controller.js
+++ Back-End/app/controllers/item.controller.js
@@ -34,8 +34,8 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  const title = req.query.first;
-  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  var name = req.query.name;
+  var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
 
   Items.findAll({ where: condition })
     .then(data => {
