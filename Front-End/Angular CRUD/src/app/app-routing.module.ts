10f97e8f6299f452806157960961555c97e65991
santosh <santosh@electems.com>
2021-06-01 17:17:10 +0530

    code commit for view of form details and template details

--- Front-End/Angular CRUD/src/app/app-routing.module.ts
+++ Front-End/Angular CRUD/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ViewTemplateComponent } from './components/view-template/view-template.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { NgModule } from '@angular/core';
@@ -13,6 +14,7 @@ import { AddProductComponent } from './components/add-product/add-forms.componen
 import { EditProductComponent } from './components/edit-forms/edit-forms.component';
 import { ProductListComponent } from './components/forms-list/forms-list.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { ViewFormDetailsComponent } from './components/view-form-details/view-form-details.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent , pathMatch: 'full' },
@@ -30,6 +32,8 @@ const routes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'homePage', component: HomePageComponent },
   { path: 'contactUs', component: ContactUsComponent },
+  { path: 'products/viewForm/:id', component:ViewFormDetailsComponent },
+  { path: 'products/template/:id', component:ViewTemplateComponent },
 ];
 
 @NgModule({
