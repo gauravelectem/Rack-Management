7508385d03d253192ff822763bb3aca3e18a735b
santoshelectem <santosh@electems.com>
2021-06-15 20:09:50 +0530

    code committed for email sent

---
+++ Front-End/src/app/add-edit-staff/add-edit-staff.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { UserService } from '../services/user.service';
+import { Staff } from '../models/staff.model';
+import swal from 'sweetalert2';
+@Component({
+  selector: 'app-add-edit-staff',
+  templateUrl: './add-edit-staff.component.html',
+  styleUrls: ['./add-edit-staff.component.css']
+})
+export class AddStaffComponent implements OnInit {
+
+  staff: Staff = {
+    username: '',
+    email: '',
+    password: '',
+  };
+  UserObj: any = {};
+  staffRoleID: '';
+  clientName: '';
+  staffObj = { username: '', email: '', password:'' };
+  constructor(  private router: Router,
+    private userService: UserService,
+    private formBuilder: FormBuilder,private route: ActivatedRoute) { }
+
+    loading = false;
+    submitted = false;
+    staffForm: FormGroup;
+  ngOnInit(): void {
+    this.UserObj = JSON.parse(sessionStorage.getItem('userObj'));
+    this.getStaffRole();
+    this.getClientName(this.UserObj.clientFk);
+    this.staffForm = this.formBuilder.group({
+      username : ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      clientFk: this.UserObj.clientFk,
+      status: 'ACTIVE',
+      roleId: this.staffRoleID,
+  });
+  this.getStaffData(this.route.snapshot.params.id);
+  }
+
+  get f() { return this.staffForm.controls; }
+
+  saveClientStaff(): void {
+    this.submitted = true;
+    if (this.staffForm.invalid) {
+      return;
+    }
+    if(this.route.snapshot.params.id) {
+      return this.updateClientStaff();
+    }
+    this.staffForm.value.roleId = this.staffRoleID;
+    this.staffForm.value.username =  this.clientName + '.' + this.staffForm.value.username;
+    this.userService.saveClientStaff(this.staffForm.value)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+          this.router.navigate(['/staff']);
+          },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateClientStaff(): void {
+    this.submitted = true;
+    if (this.staffForm.invalid) {
+      return;
+    }
+    this.staffForm.value.roleId = this.staffRoleID;
+    this.staffForm.value.username =  this.clientName + '.' + this.staffForm.value.username;
+    this.userService.updateClientStaff(this.route.snapshot.params.id,this.staffForm.value)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+          this.router.navigate(['/staff']);
+          },
+        error => {
+          console.log(error);
+        });
+  }
+
+  getStaffRole(): void {
+    this.userService.getStaffRole()
+        .subscribe(
+            data => {
+                this.staffRoleID = data[0].id;
+            },
+            error => {
+                console.log(error);
+  });
+}
+
+    getClientName(id:any): void {
+      this.userService.getClientName(id)
+          .subscribe(
+              data => {
+                  this.clientName = data[0].name;
+              },
+              error => {
+                  console.log(error);
+    });
+    }
+  
+    getStaffData(id: string): void {
+        this.userService.get(id)
+            .subscribe(
+              data => {
+                console.log(data);
+                this.staffObj = data;
+            },
+                error => {
+                    console.log(error);
+      });
+    }  
+}
