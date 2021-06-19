8f4997f55e14f2b9677c31d1dc03af806a7aa7e4
santosh <santosh@electems.com>
2021-06-19 17:36:07 +0530

    code commit for Front-End password validation with error message

--- Front-End/src/app/components/change-password/change-password.component.ts
+++ Front-End/src/app/components/change-password/change-password.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Profile } from '../../models/userProfile.model';
 import { UserProfileService } from '../../services/user-profile.service';
+import { ConfirmedValidator } from './validator';
 
 @Component({
   selector: 'app-change-password',
@@ -9,6 +11,7 @@ import { UserProfileService } from '../../services/user-profile.service';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
   id:any;
   profileObject:any;
   profile: Profile = {
@@ -18,14 +21,28 @@ export class ChangePasswordComponent implements OnInit {
   };
 
   constructor(private router: Router,private userProfile:UserProfileService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,private fb: FormBuilder) {
+      this.form = fb.group({
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]]
+      }, { 
+        validator: ConfirmedValidator('password', 'confirmPassword')
+      })
+     }
+
+     get f(){
+      return this.form.controls;
+    }
+
+    submit(){
+      this.updatePassword();
+    }
 
   ngOnInit(): void {
     this.id=this.route.snapshot.params.id;
     this.profile.user_fk=this.id;
   }
   updatePassword(): any {
-    if (this.profile.password == this.profile.confirmPassword) {
       this.userProfile.updatePassword(this.id, this.profile)
         .subscribe(
           data => {
@@ -36,7 +53,5 @@ export class ChangePasswordComponent implements OnInit {
             console.log(error);
           });
     }
-
-  }
   }
 
